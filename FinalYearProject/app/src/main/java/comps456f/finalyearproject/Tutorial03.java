c2c12d4cf77fb46ae7c6ab67e2339f32dbcf4963
oscarkmf <42922696+oscarkmf@users.noreply.github.com>
2019-04-08 15:34:39 +0800

    update images, tutorial 2,5,6 and string xml

---
+++ FinalYearProject/app/src/main/java/comps456f/finalyearproject/Tutorial03.java
@@ -0,0 +1,13 @@
+package comps456f.finalyearproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tutorial03 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial03);
+    }
+}
