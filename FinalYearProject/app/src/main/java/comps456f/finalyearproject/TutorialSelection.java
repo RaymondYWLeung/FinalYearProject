34259d54bfabbfabfe4f713e08b4ae4ca3f4e404
LeungRaymond <>
2019-04-29 20:46:13 +0800

    Tutorial update

--- FinalYearProject/app/src/main/java/comps456f/finalyearproject/TutorialSelection.java
+++ FinalYearProject/app/src/main/java/comps456f/finalyearproject/TutorialSelection.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 
 public class TutorialSelection extends AppCompatActivity implements View.OnClickListener{
 
-    private Button tutor_but[] = new Button[8];
+    private Button tutor_but[] = new Button[17];
 
 
     @Override
@@ -36,13 +36,58 @@ public class TutorialSelection extends AppCompatActivity implements View.OnClick
 
         int id = view.getId();
 
+
         if(id == R.id.tutorial_1){
             Intent act = new Intent(TutorialSelection.this,Tutorial01.class);
             startActivity(act);
         }else if(id == R.id.tutorial_2){
             Intent act = new Intent(TutorialSelection.this,Tutorial02.class);
             startActivity(act);
+        }else if(id == R.id.tutorial_3){
+            Intent act = new Intent(TutorialSelection.this,Tutorial03.class);
+            startActivity(act);
+        }else if(id == R.id.tutorial_4){
+            Intent act = new Intent(TutorialSelection.this,Tutorial04.class);
+            startActivity(act);
+        }else if(id == R.id.tutorial_5){
+            Intent act = new Intent(TutorialSelection.this,Tutorial05.class);
+            startActivity(act);
+        }else if(id == R.id.tutorial_6){
+            Intent act = new Intent(TutorialSelection.this,Tutorial06.class);
+            startActivity(act);
+        }else if(id == R.id.tutorial_7){
+            Intent act = new Intent(TutorialSelection.this,Tutorial07.class);
+            startActivity(act);
+        }else if(id == R.id.tutorial_8){
+            Intent act = new Intent(TutorialSelection.this,Tutorial08.class);
+            startActivity(act);
+        }else if(id == R.id.tutorial_9){
+            Intent act = new Intent(TutorialSelection.this,Tutorial09.class);
+            startActivity(act);
+        }else if(id == R.id.tutorial_10){
+            Intent act = new Intent(TutorialSelection.this,Tutorial10.class);
+            startActivity(act);
+        }else if(id == R.id.tutorial_11){
+            Intent act = new Intent(TutorialSelection.this,Tutorial11.class);
+            startActivity(act);
+        }else if(id == R.id.tutorial_12){
+            Intent act = new Intent(TutorialSelection.this,Tutorial12.class);
+            startActivity(act);
+        }else if(id == R.id.tutorial_13){
+            Intent act = new Intent(TutorialSelection.this,Tutorial13.class);
+            startActivity(act);
+        }else if(id == R.id.tutorial_14){
+            Intent act = new Intent(TutorialSelection.this,Tutorial14.class);
+            startActivity(act);
+        }else if(id == R.id.tutorial_15){
+            Intent act = new Intent(TutorialSelection.this,Tutorial15.class);
+            startActivity(act);
+        }else if(id == R.id.tutorial_16) {
+            Intent act = new Intent(TutorialSelection.this, Tutorial16.class);
+            startActivity(act);
+        }else if(id == R.id.tutorial_17) {
+            Intent act = new Intent(TutorialSelection.this, Tutorial17.class);
+            startActivity(act);
         }
-
     }
 }
